ec501c78c0407972c77bc6c115c8b0da58be4e39
Yvan Patry <yvan@julius.fr>
2013-02-20 12:50:55 +0100

    [FIX] - put back the field child_pack_id in the stock tracking history

--- stock_tracking_add_remove_pack/stock_tracking.py
+++ stock_tracking_add_remove_pack/stock_tracking.py
@@ -32,11 +32,14 @@ class stock_tracking(orm.Model):
             context = {}
         history_obj = self.pool.get('stock.tracking.history')
         pack = self.browse(cr, uid, pack_id, context=context)
+        child = self.browse(cr, uid, child_ids, context=context)[0]
         hist_id = history_obj.create(cr, uid, {
            'tracking_id': pack.id,
            'type': 'add_object',
            'location_id': pack.location_id.id,
            'location_dest_id': pack.location_id.id,
+           'child_pack_id': child.id,
+           'qty': 1.0,
         }, context=context)
         self.write(cr, uid, child_ids, {'parent_id': pack_id}, context=context)
         self.get_products(cr, uid, [pack_id], context=context)
@@ -48,15 +51,39 @@ class stock_tracking(orm.Model):
             context = {}
         history_obj = self.pool.get('stock.tracking.history')  
         pack = self.browse(cr, uid, pack_id, context=context)
+        child = self.browse(cr, uid, child_ids, context=context)[0]
         hist_id = history_obj.create(cr, uid, {
            'tracking_id': pack.id,
            'type': 'remove_object',
            'location_id': pack.location_id.id,
            'location_dest_id': pack.location_id.id,
+           'child_pack_id': child.id,
+           'qty': 1.0,
         }, context=context)
         self.write(cr, uid, child_ids, {'parent_id': False}, context=context)
         self.get_products(cr, uid, [pack_id], context=context)
         self.get_serials(cr, uid, [pack_id], context=context)
         return True
+<<<<<<< TREE
+    
+class stock_tracking_history(osv.osv):
+    
+    _inherit = "stock.tracking.history"
+    
+    def _get_types(self, cr, uid, context={}):
+        res = super(stock_tracking_history, self)._get_types(cr, uid, context)
+        if not res:
+            res = []
+        res = res + [('add_pack',_('Add pack')),('remove_pack',_('Remove pack'))]
+        return res
+    
+    _columns = {
+        'type': fields.selection(_get_types, 'Type'),
+        'child_pack_id': fields.many2one('stock.tracking', 'Pack'),
+        
+        
+    }
+=======
+>>>>>>> MERGE-SOURCE
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
