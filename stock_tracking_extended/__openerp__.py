9502a9ad13b8e37e453348da8d377e17d23bf0c3
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2015-03-02 17:28:25 +0100

    Add OCA as author of OCA addons
    In order to get visibility on https://www.odoo.com/apps the OCA board has
    decided to add the OCA as author of all the addons maintained as part of the
    association.

--- stock_tracking_extended/__openerp__.py
+++ stock_tracking_extended/__openerp__.py
@@ -22,7 +22,7 @@
 {
     "name" : "Stock Tracking extended",
     "version" : "1.0",
-    "author" : "Julius Network Solutions",
+    "author" : "Julius Network Solutions,Odoo Community Association (OCA)",
     "description" : """
 
 Presentation:
