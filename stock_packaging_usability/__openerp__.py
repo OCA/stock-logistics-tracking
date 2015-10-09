4e6623a29ecd6328f5b6195d55d2d52fb0c98c37
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 10:03:14 +0200

    prefix versions with 8.0

--- stock_packaging_usability/__openerp__.py
+++ stock_packaging_usability/__openerp__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'Stock Packaging Usability',
-    'version': '1.0',
+    'version': '8.0.1.0.0',
     'category': 'Warehouse Management',
     'license': 'AGPL-3',
     'summary': "Faster packaging process in Odoo",
