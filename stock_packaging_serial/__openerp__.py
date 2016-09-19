ea1074e336adc2f8c11deb8aa6dd1a1c547aa6de
Denis Roussel <denis.roussel@acsone.eu>
2016-09-19 11:20:14 +0200

    [8.0] Add gs1 serial numbers on packages for logistics

---
+++ stock_packaging_serial/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 ACSONE S.A.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Stock Packaging Serial',
+    'summary': """
+        Allows to generate serial numbers on packages through logistical units""",
+    'version': '8.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'ACSONE S.A.,Odoo Community Association (OCA)',
+    'website': 'http://acsone.eu',
+    'depends': ['stock','base_gs1_barcode'
+    ],
+    'data': [
+        'security/stock_packaging_serial.xml',
+        'views/stock_packaging_serial.xml',
+    ],
+    'demo': [
+    ],
+}
