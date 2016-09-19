cd7b63fb493350763601f07e9d1a5a7661140059
Denis Roussel <denis.roussel@acsone.eu>
2016-09-19 11:28:00 +0200

    [ADD] Add SSCC implementation

---
+++ stock_packaging_serial_sscc/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 ACSONE S.A.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Stock Packaging Serial SSCC',
+    'summary': """
+        This extends stock_packaging_serial to take SSCC generation into account""",
+    'version': '8.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'ACSONE S.A.,Odoo Community Association (OCA)',
+    'website': 'http://acsone.eu',
+    'depends': ['stock_packaging_serial', 'partner_identification_gln'
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
---
+++ stock_packaging_serial_sscc/models/stock_packaging_serial.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 ACSONE S.A.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, _
+
+
+class ProductUl(models.Model):
+    _inherit = 'product.ul'
+
+    def next_serial_00(self):
+
+        seq = self.package_sequence_id._next()
+
+        header = self.env.user.company_id.partner_id.id_numbers
+        if header:
+            length = 12 - (len(header[0].name) + len(seq))
+            if length < 0:
+
+                seq = seq[-length:]
+            else:
+                seq = ('0' * length) + seq
+
+            gs1_header = header[0].name + seq
+
+            i = 1
+            number = 0
+            for num in gs1_header:
+                if (i % 2) == 0:
+                    number += int(num * 3)
+                else:
+                    number += int(num)
+                i += 1
+
+            digit = (number % 10)
+
+            return gs1_header + str(digit)
