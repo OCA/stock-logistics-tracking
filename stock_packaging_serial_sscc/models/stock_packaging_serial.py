b752b2b567471a4c64be0de7b90c152d71ac1335
Denis Roussel <denis.roussel@acsone.eu>
2016-09-30 18:37:37 +0200

    [FIX] Flake8

--- stock_packaging_serial_sscc/models/stock_packaging_serial.py
+++ stock_packaging_serial_sscc/models/stock_packaging_serial.py
@@ -2,7 +2,7 @@
 # Copyright 2016 ACSONE S.A.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, _
+from openerp import models
 
 
 class ProductUl(models.Model):
