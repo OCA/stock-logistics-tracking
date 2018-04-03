00af673d16743dea23775b43242380713b328150
Alexis de Lattre <alexis.delattre@akretion.com>
2018-04-03 18:33:57 +0200

    Add module stock_product_ul (restore product.ul object)

---
+++ stock_product_ul/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Logistics Units',
+    'version': '10.0.1.0.0',
+    'category': 'Warehouse',
+    'license': 'AGPL-3',
+    'summary': 'Restore product.ul object',
+    'author': 'Akretion,Odoo Community Association (OCA)',
+    'website': 'http://www.akretion.com',
+    'depends': ['stock'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/product_ul.xml',
+        'views/stock_quant_package.xml',
+    ],
+    'demo': ['demo/product_ul.xml'],
+    'url': 'https://odoo-community.org/',
+    'installable': True,
+}
---
+++ stock_product_ul/models/stock_quant_package.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class StockQuantPackage(models.Model):
+    _inherit = 'stock.quant.package'
+
+    ul_id = fields.Many2one(
+        'product.ul', string='Logistics Unit', ondelete='restrict')
