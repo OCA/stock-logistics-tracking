18cd0ffcba14aa43697be3bc8f9bdb3f19ead36e
arantxa-s73 <arantxa@studio73.es>
2025-02-10 17:02:26 +0100

    [MIG] stock_quant_package_product_packaging: Migration to 18.0

--- stock_quant_package_product_packaging/__manifest__.py
+++ stock_quant_package_product_packaging/__manifest__.py
@@ -3,10 +3,10 @@
 {
     "name": "Stock Quant Package Product Packaging",
     "summary": "Use product packagings on packages",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "development_status": "Beta",
     "category": "Warehouse Management",
-    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "website": "https://github.com/OCA/stock-logistics-tracking",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
