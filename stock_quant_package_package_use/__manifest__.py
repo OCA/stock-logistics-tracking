b0a1de2d4ca22c1d52613aeb2b5fdbff0ec9b6ba
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2025-11-18 12:27:55 +0100

    Add stock_quant_package_package_use
    This module inserts the field `package_use` on
    the stock.quant.package views.
    
    The `package_use` field is created by the `stock` module,
    but only added to some views by the enterprise
    module `stock_barcode_picking_batch`.

---
+++ stock_quant_package_package_use/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+{
+    "name": "Stock Quant Pacckage Package Use",
+    "summary": "Adds the package_use field in the views",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-tracking",
+    "depends": ["stock"],
+    "data": [
+        "views/stock_quant_package_views.xml",
+    ],
+    "installable": True,
+}
---
+++ stock_quant_package_package_use/readme/DESCRIPTION.md
@@ -0,0 +1,5 @@
+This module inserts the field package_use on the stock.quant.package
+views.
+
+The package_use field is created by the stock module, but only added to
+some views by the enterprise module stock_barcode_picking_batch.
